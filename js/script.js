9a5bc9b4608eb725d7dd37c33e8a9154d32ed74f
dedsec047 <162907339+dedsec047@users.noreply.github.com>
2024-06-05 02:55:06 +0500

    Updated
    -Added complete interface of "Todos" page
    -user name showing in the top navbar
    -todos completly showing in the table
    -improved login/singup functionality

--- js/script.js
+++ js/script.js
@@ -10,12 +10,17 @@ document.getElementById("footer").innerHTML = "&copy;" +yearNow+". All Rights Re
 let users = [{email:"ahsanmushtaq172@gmail.com", pass:"123", uID: "1" , status:"active" , createdAt:"" },]
 
 // todos array
-let todos = [
-    {title: "title1", date: new Date(), id: "task1", isCompleted: false},
-    {title: "title2", date: new Date(), id: "task2", isCompleted: false},
-    {title: "title3", date: new Date(), id: "task3", isCompleted: false},
-]
+// let todos = [
+//     {title: "title1", date: new Date(), id: "task1", isCompleted: false},
+//     {title: "title2", date: new Date(), id: "task2", isCompleted: false},
+//     {title: "title3", date: new Date(), id: "task3", isCompleted: false},
+// ]
 
+let todos = [
+    {title: "title1", description: "abc", date: "01/01/2024" , id: "01" , status:"incompleted" , createdAt: now.getDay()+"/"+now.getDate()+"/"+now.getFullYear()+" - "+now.getHours()+":"+now.getMinutes() , UserID: "email" },
+    {title: "title2", description: "abc", date: "01/01/2024" , id: "02" , status:"incompleted" , createdAt: now.getDay()+"/"+now.getDate()+"/"+now.getFullYear()+" - "+now.getHours()+":"+now.getMinutes() , UserID: "email" },
+    {title: "title3", description: "abc", date: "01/01/2024" , id: "03" , status:"incompleted" , createdAt: now.getDay()+"/"+now.getDate()+"/"+now.getFullYear()+" - "+now.getHours()+":"+now.getMinutes() , UserID: "email" },
+    ]
 
 // User Registration
 const handleUserRegister = () => {
@@ -56,7 +61,9 @@ const handleUserRegister = () => {
     if(userFind){
         if(passMatch){
             showToast("user Loged in")
-            document.getElementById("todos").style.display= "none"
+            document.getElementById("log-signup").style.display= "none"
+            document.getElementById("todos-dash").style.display= "block"
+            document.getElementById("icon").innerHTML= newemail
             handleShowTable()
         }else{
             showToast("password not matched")
@@ -74,11 +81,11 @@ const handleShowTable = () => {
         showNotification("there is no single Title", "error")
     }
     let tableStartingCode = `<div class="table-responsive"><table class="table text-center">`
-    let tableHead = `<tr><th>#</th><th>Title</th><th>Date</th><th>ID</th><th>IsCompleted</th></tr>`
+    let tableHead = `<tr><th>#</th><th>Title</th><th>Discription</th><th>Date</th><th>Id</th><th>Status</th><th>CreatedAt</th><th>User</th></tr>`
     let tableEndingCode = `</table></div>`
     let tableBody = ""
     for (let i = 0; i < todos.length; i++) {
-        tableBody += `<tr><td>${i + 1}</td><td>${todos[i].title}</td><td>${todos[i].date}</td><td>${todos[i].id}</td><td>${todos[i].isCompleted}</td><td></tr>`
+        tableBody += `<tr><td>${i + 1}</td><td>${todos[i].title}</td><td>${todos[i].description}</td><td>${todos[i].date}</td><td>${todos[i].id}</td><td>${todos[i].status}</td><td>${todos[i].createdAt}</td><td>${todos[i].UserID}</td><td></tr>`
     }
     let table = tableStartingCode + tableHead + tableBody + tableEndingCode
     document.getElementById("output").innerHTML = table
@@ -110,4 +117,6 @@ function showToast(message) {
     }).showToast();
   }
 
-
+const nowtime = () => {
+    
+}
